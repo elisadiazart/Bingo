7bfddf07b611b03bb633be099e69e1a2daefcd1d
Elisa Diaz <elisadiazartcontact@gmail.com>
2023-03-16 21:59:29 +0100

    16.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,70 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-import catImage from '../assets/images/cat.jpg';
+const mainBoard = document.getElementById('mainBoard')
+const player1 = document.getElementById('player1')
+const player2 = document.getElementById('player2')
+const currentNumber = document.getElementById('currentNumber')
 
-// const img = document.createElement('img')
-// img.src = catImage
+let numberToPlay = Array(99)
+    .fill()
+    .map((_, index) => index +1);
 
-sayHello();
+
+let numbersRandomCard1 = Array(99)
+    .fill()
+    .map((_, index) => index +1);
+
+let numbersRandomCard2 = Array(99)
+    .fill()
+    .map((_, index) => index +1);
+
+
+
+
+const extractRandomNumber= array => {
+    const index = Math.floor(Math.random() * array.length)
+    const number = array[index]
+    array.splice(index, 1)
+    return number
+}
+
+const paintNumberPlayersBoard = (numberSelected) => {
+    const player1Numbers = ([...player1.children]);
+    player1Numbers.forEach(number => {
+        if(number.textContent === numberSelected)
+        console.log(`El numero seleccionado es ${number}`);
+    })
+}
+
+
+const paintNumbersMainBoard = () => {
+    const numbers =(mainBoard.children);
+    let numberRandom = extractRandomNumber(numberToPlay)
+    numbers[numberRandom].classList.add('number--selected');
+    console.log(numberRandom + 1);
+    paintNumberPlayersBoard(numberRandom + 1)
+    currentNumber.textContent = `Number: ${numberRandom + 1}`
+}
+
+paintNumbersMainBoard()
+
+
+
+const paintPlayersBoards = (board, array) => {
+    const fragment = document.createDocumentFragment()
+    for (let index = 1; index <= 15; index++) {
+        const newNumberPlayer = document.createElement('span')
+        newNumberPlayer.classList.add('number')
+        newNumberPlayer.textContent= extractRandomNumber(array)
+        fragment.append(newNumberPlayer)
+    }
+    board.append(fragment)
+}
+
+
+
+
+paintPlayersBoards(player1, numbersRandomCard1)
+paintPlayersBoards(player2, numbersRandomCard2)
+paintNumberPlayersBoard()
