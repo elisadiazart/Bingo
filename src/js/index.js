9c30603ee24c2ca2d3ec6e0f34a427070278abec
Elisa <elisadiazartcontact@gmail.com>
2023-03-17 13:07:27 +0100

    17.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ const mainBoard = document.getElementById('mainBoard')
 const player1 = document.getElementById('player1')
 const player2 = document.getElementById('player2')
 const currentNumber = document.getElementById('currentNumber')
+const button = document.getElementById('button')
 
 let numberToPlay = Array(99)
     .fill()
@@ -25,6 +26,7 @@ let timeoutId;
 
 
 
+
 const extractRandomNumber= array => {
     const index = Math.floor(Math.random() * array.length)
     const number = array[index]
@@ -32,10 +34,11 @@ const extractRandomNumber= array => {
     return number
 }
 
+
+
 const paintNumberPlayersBoard = (numberSelected) => {
     const player1Numbers = ([...player1.children]);
     const player2Numbers = ([...player2.children]);
-    console.log(numberSelected);
     player1Numbers.forEach(number => {
         if(Number(number.textContent) === numberSelected){
             number.classList.add('number--selected')
@@ -52,12 +55,11 @@ const paintNumberPlayersBoard = (numberSelected) => {
 
 
 
-const paintNumbersMainBoard = () => {
+const paintNumbersMainBoard = (numberRandom) => {
     const numbers =(mainBoard.children);
-    let numberRandom = extractRandomNumber(numberToPlay)
-    numbers[numberRandom].classList.add('number--selected');
-    paintNumberPlayersBoard(numberRandom + 1)
-    currentNumber.textContent = `Number: ${numberRandom + 1}`
+    numbers[numberRandom -1].classList.add('number--selected');
+    paintNumberPlayersBoard(numberRandom)
+    currentNumber.textContent = `Number: ${numberRandom}`
 }
 
 
@@ -76,19 +78,80 @@ const paintPlayersBoards = (board, array) => {
 }
 
 
-
+const resetArray = (array) => {
+    array = []
+    array = Array(99)
+        .fill()
+        .map((_, index) => index +1);
+}
 
 paintPlayersBoards(player1, numbersRandomCard1)
 paintPlayersBoards(player2, numbersRandomCard2)
+
+const restartGame= () => {
+    const player1Numbers = ([...player1.children]);
+    const player2Numbers = ([...player2.children]);
+    const mainBoardNumbers = ([...mainBoard.children])
+    player1Numbers.forEach(number => {
+        number.classList.remove('number--selected')
+    });
+    player2Numbers.forEach(number => {
+        number.classList.remove('number--selected')
+    });
+    mainBoardNumbers.forEach(number => {
+        number.classList.remove('number--selected')
+    });
+    currentNumber.textContent = `Number:`
+
+    numberToPlay = []
+    numberToPlay = Array(99)
+        .fill()
+        .map((_, index) => index +1);
+
+    numbersRandomCard1 = []
+    numbersRandomCard1 = Array(99)
+        .fill()
+        .map((_, index) => index +1);
+    
+    numbersRandomCard1 = []
+    numbersRandomCard1 = Array(99)
+        .fill()
+        .map((_, index) => index +1);
+
+    extractNumberEvery2Seconds()
+}
+
+
 
 
 
 const extractNumberEvery2Seconds = () => {
     clearTimeout(timeoutId);
-        paintNumbersMainBoard()
-        paintNumberPlayersBoard()
-  
+    if(!button.classList.contains('button--disabled')){button.classList.add('button--disabled')}
+        let numbersSelected1 = player1.querySelectorAll('.number--selected');
+        let numbersSelected2 = player2.querySelectorAll('.number--selected');
+        if([...numbersSelected1].length === 15 || [...numbersSelected2].length === 15){
+            button.classList.remove('button--disabled')
+            clearTimeout(timeoutId);
+            return
+        }
+        else{
+            let numberRandom = extractRandomNumber(numberToPlay)
+            paintNumbersMainBoard(numberRandom )
+        }
       timeoutId = setTimeout(extractNumberEvery2Seconds, 200);
     }
 
 extractNumberEvery2Seconds()
+
+button.addEventListener('click', () => {
+    if(button.classList.contains('button--disabled')) return
+    else restartGame()
+    
+})
+
+
+
+
+
+        
